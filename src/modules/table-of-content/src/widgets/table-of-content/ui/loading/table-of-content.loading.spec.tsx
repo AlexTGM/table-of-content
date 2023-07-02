614a31747f249619d49719fb24860e749695b036
a.mineev <a.mineev@plumsail.com>
2023-07-02 23:17:59 +0300

    add tests for loading state

---
+++ src/modules/table-of-content/src/widgets/table-of-content/ui/loading/table-of-content.loading.spec.tsx
@@ -0,0 +1,14 @@
+import { TableOfContentLoading } from "..";
+import { renderWithProviders } from "../../../../shared";
+
+describe("Table Of Content Loading should", () => {
+  it("render exact 8 elements", () => {
+    const { queryAllByTestId } = renderWithProviders(<TableOfContentLoading />);
+
+    const matchingElements = queryAllByTestId((value) =>
+      value.startsWith("skeleton")
+    );
+
+    expect(matchingElements.length).toBe(8);
+  });
+});
